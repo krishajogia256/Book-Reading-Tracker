6f9aefab4cfae82e0ddac9263943f3877b8eed78
Surya <suryanarayan0320@gmail.com>
2025-12-06 23:50:39 +0530

    Layout

---
+++ project.js
@@ -0,0 +1,106 @@
+gsap.from("#firstcard2",{
+    y : 150,
+    duration: 6,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#firstcard2",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+
+gsap.from("#firstcard1",{
+    y : 100,
+    duration: 8,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#firstcard1",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: 2,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+
+gsap.from("#secondcard1",{
+    y : 100,
+    duration: 4,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#secondcard1",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+gsap.from("#secondcard2",{
+    y : 100,
+    duration: 10,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#secondcard2",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+
+gsap.from("#thirdcard1",{
+    y : 100,
+    duration: 10,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#thirdcard1",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+gsap.from("#thirdcard2",{
+    y : 150,
+    duration: 6,
+    // delay: 0.5,
+    ease: "power2.linear",
+    scrollTrigger:{
+        trigger: "#thirdcard2",
+        scroller: 'body',
+        // markers: true,
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    
+
+})
+
